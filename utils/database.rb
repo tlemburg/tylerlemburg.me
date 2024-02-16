f5189cee7df9aac41f596d7f0af6fbe9b0c72211
Tyler Lemburg <trlemburg@gmail.com>
2024-02-16 15:18:31 -0600

    Remove DB I guess

--- utils/database.rb
+++ utils/database.rb
@@ -1,13 +1,2 @@
 require 'active_record'
 
-if ENV['RACK_ENV'] == 'production'
-  ActiveRecord::Base.establish_connection(ENV['CLEARDB_DATABASE_URL'].sub('mysql://', 'mysql2://'))
-else
-  ActiveRecord::Base.establish_connection({
-      :adapter => CONFIG["db"]["adapter"],
-      :host => CONFIG["db"]["host"],
-      :username => CONFIG["db"]["username"],
-      :password => CONFIG["db"]["password"],
-      :database => CONFIG["db"]["database"]
-  })
-end
